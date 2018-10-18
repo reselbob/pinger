95dcfea0b3e521411838693dda0fe140b50d3337
Bob Reselman <reselbob@gmail.com>
2018-10-18 15:44:49 -0700

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # pinger
-A simple Node.js app that returns information about the runtime environment
+A simple Node.js app that returns information about the runtime environment.
+
+## How to call
+
+`curl http:IP_ADDRESS:3000`
+
+WHERE `3000` is the default port.
+
+You can alter the runtime port of the app by setting the environment variable, `PINGER_PORT` to the port number you want to use.
+
+## Sample output
+
+```$xslt
+
+{
+  "APIVersion": "UNKNOWN",
+  "startTime": "2018-10-18T22:39:31.117Z",
+  "interfaces": [
+    {
+      "ifname": "en5",
+      "ipaddress": "192.168.86.103"
+    }
+  ],
+  "requestHeaders": {
+    "host": "localhost:3000",
+    "connection": "keep-alive",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9"
+  }
+}
+```
---
+++ server.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const os = require('os');
+let i = 0;
+
+const port = process.env.PINGER_PORT || 3000;
+
+function getRuntimeInfo(){
+    let interfaces = [];
+    const ifaces = os.networkInterfaces();
+    Object.keys(ifaces).forEach(function (ifname) {
+        var alias = 0;
+
+        ifaces[ifname].forEach(function (iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+            }
+
+            if (alias >= 1) {
+                // this single interface has multiple ipv4 addresses
+                interfaces.push({ifname: ifname + ':' + alias, ipaddress: iface.address})
+                //console.log(ifname + ':' + alias, iface.address);
+            } else {
+                // this interface has only one ipv4 adress
+                interfaces.push({ifname: ifname, ipaddress: iface.address})
+                //console.log(ifname, iface.address);
+            }
+            ++alias;
+        });
+    });
+    const vers = process.env.CURRENT_VERSION || 'UNKNOWN';
+
+    return {APIVersion: vers, startTime: new Date(), interfaces};
+}
+
+
+const handleRequest = function(request, response) {
+    const runtimeInfo = getRuntimeInfo();
+    runtimeInfo.requestHeaders = request.headers;
+    response.writeHead(200);
+    response.end(JSON.stringify(runtimeInfo));
+};
+
+const www = http.createServer(handleRequest);
+www.listen(port, ()=>{
+    console.log(`Listening on port ${port}`);
+});
