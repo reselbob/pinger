ee0ff696d69bd96f9b1db8831316ed6083518e21
Bob Reselman <reselbob@gmail.com>
2018-10-20 17:31:33 -0700

    adding launcher directives

--- launcher.sh
+++ launcher.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-kubectrl apply -f
+kubectl apply -f ./manifests/deployment.yaml
+kubectl apply -f ./manifests/service.yaml
+
+kubectl patch deployment ping -p '{"spec":{"template":{"spec":{"containers":[{"image":"pinger:latest","name":"pinger","env":[{"name":"CURRENT_VERSION","value":"v2"},{"name":"SECRET_MESSAGE,"value":"Kube is Cool"}]}]}}}}'
+
+#rollback the deployment to version 1
+kubectl rollout undo deployment/pinger
