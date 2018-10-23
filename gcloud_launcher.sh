47753694ada66a7bedc714802694c58d0728549e
Bob Reselman <reselbob@gmail.com>
2018-10-23 15:34:56 -0700

    revised ingress

--- gcloud_launcher.sh
+++ gcloud_launcher.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 gcloud config set project $PROJECT_ID
-gcloud auth activate-service-account $GOOGLE_ACCOUNT --key-file client-secret.json
+gcloud auth activate-service-account $GOOGLE_ACCOUNT --key-file pinger-secret.json
 gcloud container clusters get-credentials pingercluster --zone=us-central1-a
