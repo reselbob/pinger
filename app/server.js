3a791f1cdf902315254f1ea6db2b3c6cccaa0838
Bob Reselman <reselbob@gmail.com>
2018-11-01 15:00:04 -0700

    Change netInfo information

--- app/server.js
+++ app/server.js
@@ -5,46 +5,23 @@ let i = 0;
 const port = process.env.PINGER_PORT || 3000;
 
 function getRuntimeInfo(){
-    let interfaces = [];
-    const ifaces = os.networkInterfaces();
-    Object.keys(ifaces).forEach(function (ifname) {
-        var alias = 0;
-
-        ifaces[ifname].forEach(function (iface) {
-            if ('IPv4' !== iface.family || iface.internal !== false) {
-                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-                return;
-            }
-
-            if (alias >= 1) {
-                // this single interface has multiple ipv4 addresses
-                interfaces.push({ifname: ifname + ':' + alias, ipaddress: iface.address})
-                //console.log(ifname + ':' + alias, iface.address);
-            } else {
-                // this interface has only one ipv4 adress
-                interfaces.push({ifname: ifname, ipaddress: iface.address})
-                //console.log(ifname, iface.address);
-            }
-            ++alias;
-        });
-    });
     const vers = process.env.CURRENT_VERSION || 'UNKNOWN';
     const secretMessage = process.env.SECRET_MESSAGE || 'UNKNOWN';
-
-    return {APIVersion: vers, startTime: new Date(), interfaces, secretMessage, processId: process.pid};
+    return {APIVersion: vers, startTime: new Date(), netInfo:os.networkInterfaces(), secretMessage, processId: process.pid};
 }
 
 
 const handleRequest = function(request, response) {
     runtimeInfo.requestHeaders = request.headers;
     runtimeInfo.currentTime = new Date();
-    runtimeInfo.requestUrl = request.url
+    runtimeInfo.requestUrl = request.url;
+    runtimeInfo.remoteAddress = request.headers['x-forwarded-for'] || request.connection.remoteAddress
     response.writeHead(200);
     response.end(JSON.stringify(runtimeInfo, null, 4));
 };
 
 const runtimeInfo = getRuntimeInfo();
-const www = http.createServer(handleRequest);
-www.listen(port, ()=>{
+const server = http.createServer(handleRequest);
+server.listen(port, ()=>{
     console.log(`Listening on port ${port}`);
 });
