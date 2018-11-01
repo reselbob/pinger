bfeb8d5ccee4e07d9f95855db0fc9cb64f9e7628
Bob Reselman <reselbob@gmail.com>
2018-11-01 15:23:07 -0700

    Modified pinger read out

--- app/server.js
+++ app/server.js
@@ -5,9 +5,15 @@ let i = 0;
 const port = process.env.PINGER_PORT || 3000;
 
 function getRuntimeInfo(){
+    let networkInfo;
+    try {
+        networkInfo = os.networkInterfaces();
+    } catch (e) {
+        networkInfo = 'UNKNOWN';
+    }
     const vers = process.env.CURRENT_VERSION || 'UNKNOWN';
     const secretMessage = process.env.SECRET_MESSAGE || 'UNKNOWN';
-    return {APIVersion: vers, startTime: new Date(), netInfo:os.networkInterfaces(), secretMessage, processId: process.pid};
+    return {APIVersion: vers, startTime: new Date(), secretMessage, processId: process.pid, memoryUsage: process.memoryUsage(), networkInfo};
 }
 
 
@@ -15,7 +21,7 @@ const handleRequest = function(request, response) {
     runtimeInfo.requestHeaders = request.headers;
     runtimeInfo.currentTime = new Date();
     runtimeInfo.requestUrl = request.url;
-    runtimeInfo.remoteAddress = request.headers['x-forwarded-for'] || request.connection.remoteAddress
+    runtimeInfo.remoteAddress = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
     response.writeHead(200);
     response.end(JSON.stringify(runtimeInfo, null, 4));
 };
