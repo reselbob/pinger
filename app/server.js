7d40e70a052ba7392655a5abadf2d93809f9fbea
Bob Reselman <reselbob@gmail.com>
2019-08-26 16:42:21 -0700

    adding unit and coverage

--- app/server.js
+++ app/server.js
@@ -3,8 +3,25 @@ let i = 0;
 
 const port = process.env.PINGER_PORT || 3000;
 
+var getTypeParam = (request)=>{
+    let q = request.url.split('?'),result={};
+    if(q.length>=2){
+        q[1].split('&').forEach((item)=>{
+            try {
+                const key = item.split('=')[0];
+                result = item.split('=')[1];
+                if(key.toLowerCase() !== 'type') return;
+            } catch (e) {
+                console.log(`There is a problem with request url, ${request.url}`);
+                return;
+            }
+        })
+    }
+    return result;
+}
 
-function getRuntimeInfo(){
+
+function getRuntimeInfo() {
     let networkInfo;
     try {
         networkInfo = require('os').networkInterfaces();
@@ -13,24 +30,46 @@ function getRuntimeInfo(){
     }
     const vers = process.env.CURRENT_VERSION || 'UNDEFINED';
     const secretMessage = process.env.SECRET_MESSAGE || 'UNKNOWN';
-    return {APIVersion: vers, startTime: new Date(), secretMessage, processId: process.pid, memoryUsage: process.memoryUsage(), networkInfo};
+    return {
+        APIVersion: vers,
+        startTime: new Date(),
+        secretMessage,
+        processId: process.pid,
+        memoryUsage: process.memoryUsage(),
+        networkInfo
+    };
 }
 
 
-const handleRequest = function(request, response) {
+const handleRequest = function (request, response) {
+    const param = getTypeParam(request);
     runtimeInfo.envVars = process.env;
     runtimeInfo.requestHeaders = request.headers;
     runtimeInfo.currentTime = new Date();
     runtimeInfo.requestUrl = request.url;
     runtimeInfo.remoteAddress = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
-    const str = JSON.stringify(runtimeInfo, null, 4);
+
+    let rslt;
+    if(param){
+        rslt = runtimeInfo[param] ;
+    }
+    const str = JSON.stringify(rslt || runtimeInfo, null, 4);
+    response.setHeader("Content-Type", "application/json");
     response.writeHead(200);
     response.end(str);
-    console.log({app:"pinger",request,response});
+    console.log({app: "pinger", request, response});
 };
 
 const runtimeInfo = getRuntimeInfo();
 const server = http.createServer(handleRequest);
-server.listen(port, ()=>{
+server.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
+
+
+const shutdown = () => {
+    console.log(`Server shutting down at ${new Date()}`);
+    server.close()
+};
+
+module.exports = {server,shutdown};
---
+++ app/test/http-tests.js
@@ -0,0 +1,85 @@
+'use strict';
+const supertest = require('supertest');
+const chai = require('chai');
+const expect = require('chai').expect;
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+
+const {server,shutdown} = require('../server');
+
+describe('HTTP Tests: ', () => {
+    after(function () {
+        shutdown();
+    });
+    it('Can access GET ALL /', function(done){
+        //Go get all the lists
+        supertest(server)
+            .get('/?type=network-info')
+            .set('Accept', 'application/json')
+            .then((res) => {
+                expect(res.body).to.be.an('object');
+                done();
+            })
+            .catch(done);
+    });
+
+    it('Can access GET requestHeaders /', function(done){
+        //Go get all the lists
+        supertest(server)
+            .get('/?type=requestHeaders')
+            .set('Accept', 'application/json')
+            .then((res) => {
+                expect(res.body).to.be.an('object');
+                done();
+            })
+            .catch(done);
+    });
+
+    it('Can access GET envVars /', function(done){
+        //Go get all the lists
+        supertest(server)
+            .get('/?type=envVars')
+            .set('Accept', 'application/json')
+            .then((res) => {
+                expect(res.body).to.be.an('object');
+                done();
+            })
+            .catch(done);
+    });
+
+    it('Can access GET currentTime /', function(done){
+        //Go get all the lists
+        supertest(server)
+            .get('/?type=currentTime')
+            .set('Accept', 'application/json')
+            .then((res) => {
+                expect(res.body).to.be.a('string');
+                done();
+            })
+            .catch(done);
+    });
+
+    it('Can access GET requestUrl /', function(done){
+        //Go get all the lists
+        supertest(server)
+            .get('/?type=requestUrl')
+            .set('Accept', 'application/json')
+            .then((res) => {
+                expect(res.body).to.be.a('string');
+                done();
+            })
+            .catch(done);
+    });
+
+    it('Can access GET remoteAddress /', function(done){
+        //Go get all the lists
+        supertest(server)
+            .get('/?type=remoteAddress')
+            .set('Accept', 'application/json')
+            .then((res) => {
+                expect(res.body).to.be.a('string');
+                done();
+            })
+            .catch(done);
+    });
+});
